ff10375be0094e2edb866198fbd50ebfefaaa218
2101216 <115523355+2101216@users.noreply.github.com>
2023-05-16 10:44:23 +0900

    Update 2023.sql

--- 2023.sql
+++ 2023.sql
@@ -1,12 +1,13 @@
 1 ユーザー情報
 CREATE TABLE users
-(user_id  INT AUTO_INCREMENT   NOT NULL,
- user_mail 	VARCHAR(100)   NOT NULL,
- user_name     VARCHAR(50)    NOT NULL,
- user_niku   VARCHAR(50)    NOT NULL,
- user_pass    VARCHAR(30)    NOT NULL,
- user_address VARCHAR(100)   NOT NULL,
- user_number VARCHAR(20)    NOT NULL,
+(user_id  INT AUTO_INCREMENT    NOT NULL,
+ user_mail      VARCHAR(100)    NOT NULL,
+ user_name      VARCHAR(50)     NOT NULL,
+ user_niku      VARCHAR(50)     NOT NULL,
+ user_pass      VARCHAR(30)     NOT NULL,
+ user_address   VARCHAR(100)    NOT NULL,
+ user_number    VARCHAR(20)     NOT NULL,
+ user_zip       CHAR(8)         NOT NULL,
  PRIMARY KEY(user_id)
 );
 
@@ -28,17 +29,8 @@ CREATE TABLE comments
  threads_id 	  INT(11) NOT NULL,
  user_id	  INT(11)	 NOT NULL,
  post_date	  DATE		 NOT NULL,
+ comment    VARCHAR(800)    NOT NULL,
  PRIMARY KEY(comments_id),
  FOREIGN KEY(threads_id)REFERENCES threads(threads_id),
  FOREIGN KEY(user_id)REFERENCES users(user_id)
-);
-
-
-
-4 投稿履歴
-CREATE TABLE Post_historys
-(user_id INT(11) NOT NULL,
- history_id INT AUTO_INCREMENT NOT NULL,
- PRIMARY KEY(history_id),
- FOREIGN KEY(user_id) REFERENCES users(user_id)
 );
