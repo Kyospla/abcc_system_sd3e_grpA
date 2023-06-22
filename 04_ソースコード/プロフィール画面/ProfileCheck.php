0b1ccf9c752cb0915edd43201c85d697afb0b87a
Nagata0110 <2101188@s.asojuku.ac.jp>
2023-06-22 12:28:44 +0900

    Update ProfileCheck.php

--- 04_ソースコード/プロフィール画面/ProfileCheck.php
+++ 04_ソースコード/プロフィール画面/ProfileCheck.php
@@ -21,31 +21,31 @@
 
     //郵便番号の正規表現チェック
     if(preg_match("/^[0-9]{3}-[^0-9]{4}$/",$_POST['post'])){
-        $_SESSION += "郵便番号を入力してください。\n";
+        $_SESSION['errormsg'] += "郵便番号を入力してください。\n";
         $cnt++;
     }
 
     //住所が未入力だった場合の処理
     if(empty($_POST['address'])){
-        $_SESSION += "住所を入力してください。\n";
+        $_SESSION['errormsg'] += "住所を入力してください。\n";
         $cnt++;
     }
 
     //番地の正規表現チェック
     if(preg_match("/^[0-9-]+$/",$_POST['address2']) == false){
-        $_SESSION += "番地を入力してください。\n";
+        $_SESSION['errormsg'] += "番地を入力してください。\n";
         $cnt++;
     }
 
     //電話番号の正規表現チェック
     if(preg_match("/[^0-9]/",strlen($_POST['tel'] > 8)) == false){
-        $_SESSION += "電話番号を入力してください。\n";
+        $_SESSION['errormsg'] += "電話番号を入力してください。\n";
         $cnt++;
     }
 
     //メールアドレスの正規表現チェック
     if(preg_match("/^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i",$_POST['mail']) == false){
-        $_SESSION += "メールアドレスを入力してください。";
+        $_SESSION['errormsg'] += "メールアドレスを入力してください。\n";
         $cnt++;
     }
 
@@ -60,6 +60,13 @@
         $ps->bindValue(1, $_POST['name'], PDO::PARAM_STR);
         $ps->bindValue(2, $_POST['nikku'], PDO::PARAM_STR);
         $ps->bindValue(3, $_POST['post'], PDO::PARAM_STR);
+        $ps->bindvalue(4,$_POST['address'],PDO::PARAM_STR);
+        $ps->bindValue(5, $_POST['address2'], PDO::PARAM_STR);
+        $ps->bindValue(6, $_POST['tel'], PDO::PARAM_STR);
+        $ps->bindValue(7, $_POST['mail'], PDO::PARAM_STR);
+        $ps->bindvalue(8,$_SESSION['id'],PDO::PARAM_INT);
+        $ps->execute();
+        header('Location:./ProfileCheck.php');
         
 
     //チェックNGの場合の処理
