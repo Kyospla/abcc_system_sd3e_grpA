16ae4defa6d25744a47540c3c9bf266f395029bb
Nagata0110 <2101188@s.asojuku.ac.jp>
2023-06-28 11:10:16 +0900

    Update Profile.php

--- 04_ソースコード/プロフィール画面/Profile.php
+++ 04_ソースコード/プロフィール画面/Profile.php
@@ -12,12 +12,12 @@
 <title>プロフィール画面</title>
 </head>
 <body>
-  <?php
-    session_start();
-    if(isset($_SESSION['name']) == false || isset($_SESSION['id']) == false){
-      header('Location:../ログイン画面/login.php');
-    }
-  ?>  
+<?php
+    // session_start();
+    // if(isset($_SESSION['name']) == false || isset($_SESSION['id']) == false){
+    //   header('Location:../ログイン画面/login.php');
+    // }
+  ?>
   <nav class="navbar navbar-expand-md navbar-dark" aria-label="Fourth navbar example" style="background-color:#76FF60">
     <div class="container-fluid">
       <!-- <a class="navbar-brand" href="#"><img src="../img/rogo b t.png" width="20%"></a> -->
@@ -31,23 +31,34 @@
             <a class="nav-link active" aria-current="page" href="./catalog.html"><font size="5">トップ</font></a>
           </li> -->
           <li class="nav-item">
-            <a class="text-black nav-link" href="../トップ画面/Top.php">情報共有掲示板</a>
+            <a class="text-black nav-link" href="#">情報共有掲示板</a>
           </li>
           <li class="nav-item">
-            <a class="text-black nav-link" href="../投稿/Post.php">投稿</a>
+            <a class="text-black nav-link" href="#">投稿</a>
           </li>
           <li class="nav-item">
-            <a class="text-black nav-link " href="../投稿/History.php">投稿履歴</a>
+            <a class="text-black nav-link " href="./login.html">投稿履歴</a>
           </li>
         </ul>
-        <a href="../プロフィール修正/Profile.php" class="name"><?php echo $_SESSION['name'] ?></a>
-        <form role="search" action="../トップ画面/Search.php" method="post">
-          <input class="form-control" type="search" placeholder="タイトル検索" aria-label="Search" name="search">
+        <a href="#" class="name">名無しさん</a>
+        <form role="search">
+          <input class="form-control" type="search" placeholder="タイトル検索" aria-label="Search">
         </form>
       </div>
     </div>
   </nav>
+  <?php
+        $pdo = new PDO('mysql:host=mysql213.phy.lolipop.lan;dbname=LAA1418543-hiroyuki;charset=utf8','LAA1418543', '12345hiroyuki');
+        $sql="SELECT * FROM users WHERE user_id = 1";
+        $ps = $pdo ->prepare($sql);
+        //$ps -> bindValue(1,$_SESSION['id'],PDO::PARAM_STR);
+        $ps -> execute();
+        $searchArray = $ps -> fetchAll();
+      ?>
       <div class = "row">
+      <?php 
+           foreach($searchArray as $row){
+          ?>
         <div class = "col-5 text-end">
           <h1 class="mb-2 mt-3">プロフィール</h1>
         </div>
@@ -55,83 +66,68 @@
           <input type="submit" class="btn btn-danger" value="ログアウト">
         </div>
       </div>
+      <div class="row">
+        <div class="col-5">
+          <h3 class="text-end">氏名:</h3>
+        </div>
+        <div class="col-7">
+            <input type="text" class="box"  name="name"  value="<?php echo $row['user_name']?>" readonly>
+        </div>
 
-      <?php
-        $pdo = new PDO('mysql:host=mysql213.phy.lolipop.lan;dbname=LAA1418543-hiroyuki;charset=utf8','LAA1418543', '12345hiroyuki');
-        $sql="SELECT * FROM users WHERE user_id = ?";
-        $ps = $pdo ->prepare($sql);
-        $ps -> bindValue(1,$_SESSION['id'],PDO::PARAM_STR);
-        $ps -> execute();
-        $result = $ps -> fetchAll();
-      ?>
-      
-        <div class="row">
-          
-          <form action="../プロフィール修正/Profilefix.php" method="post">
-          <?php 
-           foreach($result as $row){
-          ?>
-            <div class="col-5">
-              <h3 class="text-end">氏名:</h3>
-            </div>
-            <div class="col-7">
-                <input type="text" class="box"  name="name"  value="<?php echo $row['user_name']?>" readonly>
-            </div>
+        <div class="col-5">
+          <h3 class="text-end">ニックネーム:</h3>
+        </div>
+        <div class="col-7">
+            <input type="text" class="box"  name="nikku" value="<?php echo $row['user_niku']?>" readonly>
+        </div>
 
-            <div class="col-5">
-              <h3 class="text-end">ニックネーム:</h3>
-            </div>
-            <div class="col-7">
-                <input type="text" class="box"  name="nikku" value="<?php echo $row['user_niku']?>" readonly>
-            </div>
+        <div class="col-5">
+          <h3 class="text-end">〒:</h3>
+        </div>
+        <div class="col-7">
+            <input type="text" class="box"  name="post" value="<?php echo $row['user_zip']?>" readonly>
+        </div>
 
-            <div class="col-5">
-              <h3 class="text-end">〒:</h3>
-            </div>
-            <div class="col-7">
-                <input type="text" class="box"  name="post" value="<?php echo $row['user_zip']?>" readonly>
-            </div>
+        <div class="col-5">
+          <h3 class="text-end">住所:</h3>
+        </div>
+        <div class="col-7">
+            <input type="text" class="box"  name="address" value="<?php echo $row['user_address']?>" readonly>
+        </div>
 
-            <div class="col-5">
-              <h3 class="text-end">住所:</h3>
-            </div>
-            <div class="col-7">
-                <input type="text" class="box"  name="address" value="<?php echo $row['user_address']?>" readonly>
-            </div>
+        <div class="col-5">
+          <h3 class="text-end">番地:</h3>
+        </div>
+        <div class="col-7">
+            <input type="text" class="box"  name="address2" value="<?php echo $row['user_banchi']?>" readonly>
+        </div>
 
-            <div class="col-5">
-              <h3 class="text-end">番地:</h3>
-            </div>
-            <div class="col-7">
-                <input type="text" class="box"  name="address2" value="<?php echo $row['user_banchi']?>" readonly>
-            </div>
+        <div class="col-5">
+          <h3 class="text-end">電話番号:</h3>
+        </div>
+        <div class="col-7">
+            <input type="tel" class="box"  name="tel" value="<?php echo $row['user_number']?>" readonly>
+        </div>
 
-            <div class="col-5">
-              <h3 class="text-end">電話番号:</h3>
-            </div>
-            <div class="col-7">
-                <input type="tel" class="box"  name="tel" value="<?php echo $row['user_number']?>" readonly>
-            </div>
+        <div class="col-5">
+          <h3 class="text-end">メールアドレス:</h3>
+        </div>
+        <div class="col-7">
+            <input type="email" class="box"  name="mail" value="<?php echo $row['user_mail']?>" readonly>
+        </div>
 
-            <div class="col-5">
-              <h3 class="text-end">メールアドレス:</h3>
-            </div>
-            <div class="col-7">
-                <input type="email" class="box"  name="mail" value="<?php echo $row['user_mail']?>" readonly>
-            </div>
-
-            <div class="col-5">
-              <h3 class="text-end">パスワード:</h3>
-            </div>
-            <div class="col-7">
-                <input type="password" class="box"  name="pass" value="<?php echo $row['user_pass']?>" readonly>
-                <input type="submit" href="../プロフィール修正/CheckPass.php" class="btn btn-warning text-white" value="変更する">
-            </div>
-            <input type="submit"  class="btn btn-primary mt-3 offset-5 col-2" value="修正する">
-            <?php 
+        <div class="col-5">
+          <h3 class="text-end">パスワード:</h3>
+        </div>
+        <div class="col-7">
+            <input type="password" class="box"  name="pass" value="<?php echo $row['user_pass']?>" readonly>
+            <input type="submit" class="btn btn-warning text-white" value="変更する">
+        </div>
+        <input type="submit"  class="btn btn-primary mt-3 offset-5 col-2" value="修正する">
+        <?php 
               }
             ?>
-          </form>
-        </div>
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    </div>
+     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
+</html>
