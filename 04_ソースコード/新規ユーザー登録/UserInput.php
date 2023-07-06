2c49d3b78e51facdb715f8153000d8f9285921c8
2101216 <115523355+2101216@users.noreply.github.com>
2023-07-06 10:08:57 +0900

    仮

--- 04_ソースコード/新規ユーザー登録/UserInput.php
+++ 04_ソースコード/新規ユーザー登録/UserInput.php
@@ -40,36 +40,102 @@
   </nav>
 
 
-<h1 style="text-align:center;margin-top: 2em;margin-bottom: 1em;" class="h1_log">新規ユーザー登録</h1>
-    <form method="post" action="register.php">
-        <label for="username">氏名:</label>
-        <input type="text" name="username" id="username" required><br><br>
-
-        <label for="userniku">ニックネーム:</label>
-        <input type="text" name="userniku" id="userniku" required><br><br>
-
-        <label for="userzip">〒:</label>
-        <input type="text" name="userzip" id="userzip" required><br><br>
-
-        <label for="useraddress">住所:</label>
-        <input type="text" name="useraddress" id="useraddress" required><br><br>
+  <?php
+        $pdo = new PDO('mysql:host=mysql213.phy.lolipop.lan;dbname=LAA1418543-hiroyuki;charset=utf8','LAA1418543', '12345hiroyuki');
+        $sql="SELECT * FROM users WHERE user_id = ?";
+        $ps = $pdo ->prepare($sql);
+        $ps -> bindValue(1,$_SESSION['id'],PDO::PARAM_INT);
+        $ps -> execute();
+        $searchArray = $ps -> fetchAll();
+      ?>
+      <div class = "row">
+        <div class = "col-5 text-end">
+          <h1 class="mb-2 mt-3">新規ユーザー登録</h1>
+        </div>
         
-        <label for="useraddress">番地:</label>
-        <input type="text" name="useraddress" id="useraddress" required><br><br>
-
-        <label for="usernumber">電話番号:</label>
-        <input type="text" name="usernumber" id="usernumber" required><br><br>
-
-        <label for="usermail">メールアドレス:</label>
-        <input type="mail" name="usermail" id="usermail" required><br><br>
-
-        <label for="userpass">パスワード:</label>
-        <input type="userpass" name="userpass" id="userpass" required><br><br>
+      </div>
+      
+      <div class="row">
+        <?php 
+           foreach($searchArray as $row){
+        ?>
+        <div class="offset-5 col-5">
+        <form action="../UserInput/UserInput.php" method="post">
+            <p class="error">
+              <?php
+                if(isset($_SESSION['errormsg'])){
+                  echo nl2br($_SESSION['errormsg']);
+                  unset($_SESSION['errormsg']);
+                }
+              ?>
+            </p>
+        </div>
         
-        <label for="userpass">パスワード再確認:</label>
-        <input type="userpass" name="userpass" id="userpass" required><br><br>
+        <div class="col-5">
+          <h3 class="text-end">氏名:</h3>
+        </div>
+        <div class="col-7">
+            <input type="text" class="box"  name="name"  value="<?php echo $row['user_name']?>">
+        </div>
 
-        <input type="submit" value="登録">
-    </form>
+        <div class="col-5">
+          <h3 class="text-end">ニックネーム:</h3>
+        </div>
+        <div class="col-7">
+            <input type="text" class="box"  name="nikku" value="<?php echo $row['user_niku']?>">
+        </div>
+
+        <div class="col-5">
+          <h3 class="text-end">〒:</h3>
+        </div>
+        <div class="col-7">
+            <input type="text" class="box"  name="post" value="<?php echo $row['user_zip']?>">
+        </div>
+
+        <div class="col-5">
+          <h3 class="text-end">住所:</h3>
+        </div>
+        <div class="col-7">
+            <input type="text" class="box"  name="address" value="<?php echo $row['user_address']?>">
+        </div>
+
+        <div class="col-5">
+          <h3 class="text-end">番地:</h3>
+        </div>
+        <div class="col-7">
+            <input type="text" class="box"  name="address2" value="<?php echo $row['user_banchi']?>">
+        </div>
+
+        <div class="col-5">
+          <h3 class="text-end">電話番号:</h3>
+        </div>
+        <div class="col-7">
+            <input type="tel" class="box"  name="tel" value="<?php echo $row['user_number']?>">
+        </div>
+
+        <div class="col-5">
+          <h3 class="text-end">メールアドレス:</h3>
+        </div>
+        <div class="col-7">
+            <input type="email" class="box"  name="mail" value="<?php echo $row['user_mail']?>">
+        </div>
+
+        <div class="col-5">
+          <h3 class="text-end">パスワード:</h3>
+        </div>
+        <div class="col-7">
+            <input type="email" class="box"  name="mail" value="<?php echo $row['user_pass']?>">
+        </div>
+
+        <div class="mt-3">
+          <input type="submit"  class="btn btn-primary offset-4 col-2"  value="登録する">
+        
+      </div>
+      </form>
+      <?php 
+              }
+      ?>
+        
+    </div>     
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
-</html>
