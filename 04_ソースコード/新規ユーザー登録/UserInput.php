300f140d2189a0215b4311c521d0c942ebde4701
2101216 <115523355+2101216@users.noreply.github.com>
2023-07-12 11:42:17 +0900

    仮

--- 04_ソースコード/新規ユーザー登録/UserInput.php
+++ 04_ソースコード/新規ユーザー登録/UserInput.php
@@ -10,92 +10,131 @@
     <link rel="stylesheet" href="./css/style.css">
     <title>新規ユーザー登録</title>
 </head>
+
+<?php
+// データベースの接続情報
+$servername = "mysql213.phy.lolipop.lan";
+$username = "LAA1418543";
+$password = "12345hiroyuki";
+$dbname = "LAA1418543-hiroyuki";
+
+// データベースに接続
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("データベースに接続できませんでした: " . $e->getMessage());
+}
+
+// 格納するデータ
+$name = 'John Doe';
+$email = 'johndoe@example.com';
+$age = 25;
+
+// SQLクエリの作成と実行
+try {
+    $stmt = $pdo->prepare("INSERT INTO users (name, email, age) VALUES (:name, :email, :age)");
+    $stmt->bindParam(':name', $name);
+    $stmt->bindParam(':email', $email);
+    $stmt->bindParam(':age', $age);
+    $stmt->execute();
+    echo "データを格納しました。";
+} catch (PDOException $e) {
+    die("データを格納できませんでした: " . $e->getMessage());
+}
+
+// データベース接続のクローズ
+$pdo = null;
+?>
+
 <body>
-      <div class = "row">
-        <div class = "col-5 text-end">
-          <h1 class="mb-2 mt-3">新規ユーザー登録</h1>
-        </div>
-        
-      </div>
-      
-      <div class="row">
-        <div class="offset-5 col-5">
-        <form action="./UserInput/UserCheck.php" method="post">
-            <p class="error">
-              <?php
-                if(isset($_SESSION['errormsg'])){
-                  echo nl2br($_SESSION['errormsg']);
-                  unset($_SESSION['errormsg']);
-                }
-              ?>
-            </p>
-        </div>
+    <div id="app">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12" style="background-color:#76FF60">
+                    <h1 class="text-center p-2">情報共有掲示板</h1>
+                </div>
+                <p class="text-center">エラー表示</p>
+                
+                <div class="col-12">
+                    <h1 class="text-center mt-3 mb-5">新規ユーザー登録</h1>
+                </div>
+                <div class="col-5">
+                    <h3 class="text-end">氏名:</h3>
+                </div>
+                <div class="col-7">
+                    <input type="text" class="inputbox" v-model="name" name="name" placeholder="氏名を入力"/>
+                </div>
+                <p v-if="isInValidName" class="error offset-5 col-7 mb-1">※氏名を入力してください。</p>
 
-        <div class="col-5">
-          <h3 class="text-end">氏名:</h3>
-        </div>
-        <div class="col-7">
-            <input type="text" class="box"  name="name"  value="<?php echo $row['user_name']?>">
-        </div>
+                <div class="col-5">
+                    <h3 class="text-end">ニックネーム:</h3>
+                </div>
+                <div class="col-7">
+                    <input type="text" class="inputbox" v-model="nikku" name="nikku" placeholder="サイトに表示する名前を入力"/>
+                </div>
+                <p v-if="isInValidNikku" class="error offset-5 col-7 mb-1">※ニックネームを入力してください</p>
 
-        <div class="col-5">
-          <h3 class="text-end">ニックネーム:</h3>
-        </div>
-        <div class="col-7">
-            <input type="text" class="box"  name="nikku" value="<?php echo $row['user_niku']?>">
-        </div>
+                <div class="col-5">
+                    <h3 class="text-end">〒:</h3>
+                </div>
+                <div class="col-7">
+                    <input type="text" class="inputbox" v-model="post" name="post" placeholder="例：810-0000"/>
+                </div>
+                <p v-if="isInValidPost" class="error offset-5 col-7 mb-1">※郵便番号を入力してください。</p>
 
-        <div class="col-5">
-          <h3 class="text-end">〒:</h3>
-        </div>
-        <div class="col-7">
-            <input type="text" class="box"  name="post" value="<?php echo $row['user_zip']?>">
-        </div>
+                <div class="col-5">
+                    <h3 class="text-end">住所:</h3>
+                </div>
+                <div class="col-7">
+                    <input type="text" class="inputbox" v-model="address" name="address" placeholder="例：福岡県福岡市博多区博多駅南"/>
+                </div>
+                <p v-if="isInValidaddress" class="error offset-5 col-7 mb-1">※住所を入力してください。</p>
 
-        <div class="col-5">
-          <h3 class="text-end">住所:</h3>
-        </div>
-        <div class="col-7">
-            <input type="text" class="box"  name="address" value="<?php echo $row['user_address']?>">
-        </div>
+                <div class="col-5">
+                    <h3 class="text-end">番地:</h3>
+                </div>
+                <div class="col-7">
+                    <input type="text" class="inputbox" v-model="address2" name="address2" placeholder="例：2-12-32"/>
+                </div>
+                <p v-if="isInValidaddress2" class="error offset-5 col-7 mb-1">※番地を入力してください。<br>　半角数字と半角記号-のみしか使えません。</p>
 
-        <div class="col-5">
-          <h3 class="text-end">番地:</h3>
-        </div>
-        <div class="col-7">
-            <input type="text" class="box"  name="address2" value="<?php echo $row['user_banchi']?>">
-        </div>
+                <div class="col-5">
+                    <h3 class="text-end">電話番号:</h3>
+                </div>
+                <div class="col-7">
+                    <input type="tel" class="inputbox" v-model="tel" name="tel" placeholder="01203456789"/>
+                </div>
+                <p v-if="isInValidaTel" class="error offset-5 col-7 mb-1">※電話番号を入力してください。<br>　-は入れないでください。</p>
 
-        <div class="col-5">
-          <h3 class="text-end">電話番号:</h3>
+                <div class="col-5">
+                    <h3 class="text-end">メールアドレス:</h3>
+                </div>
+                <div class="col-7">
+                    <input type="email" class="inputbox" v-model="mail" name="mail" placeholder="name@example.com"/>
+                </div>
+                <p v-if="isInValidaMail" class="error offset-5 col-7 mb-1">※メールアドレスを入力してください。</p>
+                
+                <p class="textmessage offset-5 col-7 mb-0">半角英数字と記号（.?/-@）のみ使用可能</p>
+                <div class="col-5">
+                    <h3 class="text-end">パスワード:</h3>
+                </div>
+                <div class="col-7">
+                    <input type="password" class="inputbox" v-model="pass" name="pass" placeholder="パスワードを入力"/>
+                </div>
+                <p v-if="isInValidaPass" class="error offset-5 col-7 mb-1">※パスワードを8文字以上24文字以内で入力してください。</p>
+                <div class="col-5">
+                    <h3 class="text-end">パスワード再確認:</h3>
+                </div>
+                <div class="col-7">
+                    <input type="password" class="inputbox" v-model="passcheck" name="passcheck"/>
+                </div>
+                <p v-if="isInValidaPassCheck" class="error offset-5 col-7">※パスワードが一致していません。</p>
+                <input type="submit"  class="btn btn-warning mt-2 mb-4 offset-5 col-2" value="登録する">
+            </div>
         </div>
-        <div class="col-7">
-            <input type="tel" class="box"  name="tel" value="<?php echo $row['user_number']?>">
-        </div>
-
-        <div class="col-5">
-          <h3 class="text-end">メールアドレス:</h3>
-        </div>
-        <div class="col-7">
-            <input type="email" class="box"  name="mail" value="<?php echo $row['user_mail']?>">
-        </div>
-
-        <div class="col-5">
-          <h3 class="text-end">パスワード:</h3>
-        </div>
-        <div class="col-7">
-            <input type="text" class="box"  name="pass" value="<?php echo $row['user_pass']?>">
-        </div>
-
-      <div class="mt-3">
-        <input type="submit"  class="btn btn-primary offset-4 col-2"  value="登録する">
-        
-      </div>
-      </form> 
-      <?php 
-              }
-      ?>
-        
-    </div>     
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    </div>
+    <script src="./script/script.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
 </body>
+</html>
